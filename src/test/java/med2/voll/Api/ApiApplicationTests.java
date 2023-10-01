9082c3f97b8c29e879ea210788f50aa4d002e3a7
João <joaovicenterangel@gmail.com>
2023-10-01 14:09:53 -0300

    Adicionando arquivos iniciais de configuração do projeto

---
+++ src/test/java/med2/voll/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med2.voll.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
